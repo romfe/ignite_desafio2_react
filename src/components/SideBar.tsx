2b5ad831c23d8b8e0de2a27097eee56d154460f2
Rômulo <romfe89@gmail.com>
2021-08-27 18:17:02 -0300

    primeiro componente feito

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface Props {
+  genres:
+  {
+    id: number,
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family',
+    title: string,
+  }[],
+  selectedId: number,
+  onHandleClickButton(arg: number): void
+}
+
+
+export function SideBar(props: Props) {
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    props.onHandleClickButton(parseInt(e.currentTarget.name));
+  }
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={handleClick}
+            selected={props.selectedId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
