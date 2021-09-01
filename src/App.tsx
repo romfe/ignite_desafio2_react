e9b3037a200eb69c2d6fd352deabc8190f0520e0
Rômulo <romfe89@gmail.com>
2021-09-01 08:20:25 -0300

    primeira entrega

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
+//import { Button } from './components/Button';
+//import { MovieCard } from './components/MovieCard';
 
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -65,20 +65,11 @@ export function App() {
         selectedId={selectedGenreId}
         onHandleClickButton={handleClickButton}
       />
+      <Content
+        selecionado={selectedGenre.title}
+        filmes={movies}
+      />
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
     </div>
   )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
+
+interface Props {
+  selecionado: string,
+  filmes: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }[]
+}
+
+export function Content(props: Props) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selecionado}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.filmes.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
